e333d331a788b47a825b6c31a0d596a1a0be4d96
Raed Deara <raed_6@hotmail.co.uk>
2023-03-14 18:02:34 +0000

    changed

--- getapi.js
+++
@@ -1,5 +0,0 @@
-fetch("https://hp7olzeabi.execute-api.us-east-1.amazonaws.com/prod/visitors")
-.then(response => response.json())
-.then(data => {
-        document.getElementById('replaceme').innerText = data
-})
