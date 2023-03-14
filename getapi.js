a209d27d2a1a9c8cb763c632307874a5bba08cf0
Raed Deara <raed_6@hotmail.co.uk>
2023-03-14 17:38:06 +0000

    changed

--- getapi.js
+++ getapi.js
@@ -1,20 +1,5 @@
-<body onload="updateCounter()">
-
-    <script>
-          function updateCounter(){
-              fetch('https://go9gn0766m.execute-api.us-east-1.amazonaws.com/prod/',{
-                  method: 'GET'
-              })
-            .then(response => {
-              if (
-                  response.ok 
-              ) {
-                return response.json()
-              } else {
-                throw new Error('Sorry, something went wrong');
-              }
-            })
-            .then(data => document.getElementById("hits").innerText = data.Visit_Count)
-          }
-    </script>
-    </body>
+fetch("https://hp7olzeabi.execute-api.us-east-1.amazonaws.com/prod/visitors")
+.then(response => response.json())
+.then(data => {
+        document.getElementById('replaceme').innerText = data
+})
